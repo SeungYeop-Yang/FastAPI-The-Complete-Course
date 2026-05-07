1e9d60901d2e80d690aaa991b63e9cd571decdd2
SeungYeop Yang <seungyeop.yang@gmail.com>
2026-05-07 01:13:57 -0500

    pydantic

--- Project 02/books2.py
+++ Project 02/books2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,6 +19,14 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
 BOOKS = [
     Book(1, "Computer Science Pro", "codingwithdaddy", "A very nice book!", 5),
     Book(2, "Be Fast with FastAPI", "codingwithdaddy", "A great book!", 5),
@@ -28,6 +37,21 @@ BOOKS = [
 ]
 
 
+def print_pairs(**kwargs):
+    result = ", ".join(f"{k}: {v}" for k, v in kwargs.items())
+    print(result)
+
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    print(type(book_request))
+    print(book_request.model_dump())  # dict
+    print_pairs(**book_request.model_dump())  # key value pairs
+    # new_book = Book(**book_request.dict())
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
